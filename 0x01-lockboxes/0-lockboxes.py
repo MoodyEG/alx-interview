1a2e80d57f32488fa7ddc26ce0b4e1a385e52f00
MoodyEG <moodyeg2011@yahoo.com>
2024-08-01 16:13:26 +0300

    Adding lockbox

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" LockBox """
+
+
+def canUnlockAll(boxes: list[list]):
+    """ canUnlockAll """
+    keys = set()
+    keys.add(0)
+    change = True
+    while change:
+        change = False
+        for i, box in enumerate(boxes):
+            if i in keys:
+                for key in box:
+                    if key < len(boxes) and key not in keys:
+                        keys.add(key)
+                        change = True
+                        if len(keys) == len(boxes):
+                            return True
+    return False
