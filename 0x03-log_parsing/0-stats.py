90f28c5dcdf9af1c9d285c4911240f9719528a35
MoodyEG <moodyeg2011@yahoo.com>
2024-08-15 15:38:48 +0300

    Testing 0.5

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,28 +15,26 @@ def print_stats(size, status):
 
 
 if __name__ == "__main__":
-    status = {200: 0, "301": 0, "400": 0,
+    status = {"200": 0, "301": 0, "400": 0,
               "401": 0, "403": 0, "404": 0,
               "405": 0, "500": 0}
     size = 0
     count = 0
     try:
         for line in sys.stdin:
+            count += 1
             line = line.strip()
-            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) -' + \
+            pattern = re.compile(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) -' + \
                       r' (\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6}\]) ' + \
-                      r'"GET /projects/260 HTTP/1.1" (\d{1,3}) (\d{1,4})$'
-            if not re.fullmatch(pattern, line):
+                      r'"GET /projects/260 HTTP/1.1" (\d{1,3}) (\d{1,4})$')
+            if not pattern.fullmatch(line):
                 continue
             line = line.split()
             if line[-2] not in status:
                 continue
-            if count == 10:
-                print_stats(size, status)
-                count = 1
-            else:
-                count += 1
             size += int(line[-1])
             status[line[-2]] += 1
+            if count % 10 == 0:
+                print_stats(size, status)
     finally:
         print_stats(size, status)
