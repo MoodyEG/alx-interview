595987dbd21a0023e6eee14f9c7933b1faa5a6c2
MoodyEG <moodyeg2011@yahoo.com>
2024-08-16 03:17:27 +0300

    Testing 0.8.7

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,26 +15,29 @@ def print_stats(size: int, status: dict) -> None:
 
 
 if __name__ == "__main__":
-    status = {200: 0, 301: 0, 400: 0,
-              401: 0, 403: 0, 404: 0,
-              405: 0, 500: 0}
+    status = {"200": 0, "301": 0, "400": 0,
+              "401": 0, "403": 0, "404": 0,
+              "405": 0, "500": 0}
     size = 0
     count = 0
     try:
         for line in sys.stdin:
-            line = line.strip()
-            pat = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - ' + \
-                  r'\[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d{1,6}\]' +\
-                  r' "GET /projects/260 HTTP/1.1" (\d{1,3}) (\d{1,4})$'
-            if not re.fullmatch(pat, line):
-                continue
-            line = line.split()
-            if int(line[-2]) in status:
-                count += 1
-                status[int(line[-2])] += 1
-                size += int(line[-1])
-            if count % 10 == 0:
-                print_stats(size, status)
+            try:
+                line = line.strip()
+                pat = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - ' + \
+                    r'\[\d{4}-\d{1,2}-\d{1,2} \d{2}:\d{2}:\d{2}\.\d{1,6}\]' +\
+                    r' "GET /projects/260 HTTP/1.1" (\d{1,3}) (\d{1,4})$'
+                if not re.fullmatch(pat, line):
+                    continue
+                line = line.split()
+                if line[-2] in status:
+                    count += 1
+                    status[line[-2]] += 1
+                    size += int(line[-1])
+                if count % 10 == 0:
+                    print_stats(size, status)
+            except Exception:
+                pass
     except KeyboardInterrupt:
         pass
     finally:
