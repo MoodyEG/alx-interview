a960ae5c20b71c0d7c4acbffc678b516e0a5162b
MoodyEG <moodyeg2011@yahoo.com>
2024-08-19 08:17:41 +0300

    Added Task 0 for later testing

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data: list) -> bool:
+    """ UTF-8 Validation """
+
+    for i in range(len(data)):
+        if data[i] >> 7 == 0:
+            continue
+        elif i + 1 >= len(data) and data[i] >> 5 == 6\
+                and data[i + 1] >> 6 == 2:
+            i += 1
+            continue
+        elif i + 2 >= len(data) and data[i] >> 4 == 14\
+                and data[i + 1] >> 6 == 2\
+                and data[i + 2] >> 6 == 2:
+            i += 2
+            continue
+        elif i + 3 >= len(data) and data[i] >> 3 == 30 \
+                and data[i + 1] >> 6 == 2\
+                and data[i + 2] >> 6 == 2\
+                and data[i + 3] >> 6 == 2:
+            i += 3
+            continue
+        else:
+            return False
+
+    return True
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+License and unicode transformation please
