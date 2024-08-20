ceca759d6f0a40faf6132021dd07c0f1bd6f45a1
MoodyEG <moodyeg2011@yahoo.com>
2024-08-20 11:10:52 +0300

    Testing 0.3

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,16 +9,16 @@ def validUTF8(data):
         if data[i] >> 7 == 0:
             i += 1
             continue
-        elif i + 1 <= len(data) and data[i] >> 5 == 6\
+        elif i + 1 < len(data) and data[i] >> 5 == 6\
                 and data[i + 1] >> 6 == 2:
             i += 2
             continue
-        elif i + 2 <= len(data) and data[i] >> 4 == 14\
+        elif i + 2 < len(data) and data[i] >> 4 == 14\
                 and data[i + 1] >> 6 == 2\
                 and data[i + 2] >> 6 == 2:
             i += 3
             continue
-        elif i + 3 <= len(data) and data[i] >> 3 == 30 \
+        elif i + 3 < len(data) and data[i] >> 3 == 30 \
                 and data[i + 1] >> 6 == 2\
                 and data[i + 2] >> 6 == 2\
                 and data[i + 3] >> 6 == 2:
