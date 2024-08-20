7709b0a4d49e6fb8c27be8cd556c718ca28bb432
MoodyEG <moodyeg2011@yahoo.com>
2024-08-20 10:51:49 +0300

    Testing 0.2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,27 +4,25 @@
 
 def validUTF8(data):
     """ UTF-8 Validation """
-
-    if not data:
-        return False
-
-    for i in range(len(data)):
+    i = 0
+    while i < len(data):
         if data[i] >> 7 == 0:
-            continue
-        elif i + 1 > len(data) and data[i] >> 5 == 6\
-                and data[i + 1] >> 6 == 2:
             i += 1
             continue
-        elif i + 2 > len(data) and data[i] >> 4 == 14\
-                and data[i + 1] >> 6 == 2\
-                and data[i + 2] >> 6 == 2:
+        elif i + 1 <= len(data) and data[i] >> 5 == 6\
+                and data[i + 1] >> 6 == 2:
             i += 2
             continue
-        elif i + 3 > len(data) and data[i] >> 3 == 30 \
+        elif i + 2 <= len(data) and data[i] >> 4 == 14\
+                and data[i + 1] >> 6 == 2\
+                and data[i + 2] >> 6 == 2:
+            i += 3
+            continue
+        elif i + 3 <= len(data) and data[i] >> 3 == 30 \
                 and data[i + 1] >> 6 == 2\
                 and data[i + 2] >> 6 == 2\
                 and data[i + 3] >> 6 == 2:
-            i += 3
+            i += 4
             continue
         else:
             return False
