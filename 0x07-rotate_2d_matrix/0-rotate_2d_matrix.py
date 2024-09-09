0163a33629210d8e3497cbaf15a9d77a525cc770
MoodyEG <moodyeg2011@yahoo.com>
2024-09-09 13:53:41 +0300

    Rotate

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Rotate Matrix """
+
+
+def rotate_2d_matrix(matrix: list[list]) -> None:
+    """ Rotate Matrix """
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            # Take the top left to temp
+            temp = matrix[i][j]
+            # Copy bottom left to top left
+            matrix[i][j] = matrix[n - j - 1][i]
+            # Copy bottom right to bottom left
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            # Copy top right to bottom right
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            # Copy top left to top right
+            matrix[j][n - i - 1] = temp
---
+++ 0x07-rotate_2d_matrix/REDAME.md
@@ -0,0 +1,1 @@
+Go baron, if they came, rotate
