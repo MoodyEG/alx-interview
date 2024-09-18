2f2bf4f5e2646211e2dd53e56284208a5ca131a2
MoodyEG <moodyeg2011@yahoo.com>
2024-09-18 16:05:23 +0300

    Testing 0.1.2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,11 +8,11 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for coin in coins:
         for j in range(coin, total + 1):
             dp[j] = min(dp[j], dp[j - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
