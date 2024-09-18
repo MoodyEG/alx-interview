8936225f832fee313ee0deefdb05930ffe01f7f3
MoodyEG <moodyeg2011@yahoo.com>
2024-09-18 15:01:41 +0300

    Testing 0.1

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,19 +3,16 @@
 
 
 def makeChange(coins, total):
-  """ Returns the fewest number of coins needed to meet total """
+    """ Returns the fewest number of coins needed to meet total """
 
-  if total <= 0:
-    return 0
+    if total <= 0:
+        return 0
 
-  coins.sort(reverse=True)
-  dp = [total + 1] * (total + 1)
-  dp[0] = 0
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
 
-  for j in range(1, total + 1):
     for coin in coins:
-      if j >= coin:
-        dp[j] = min(dp[j], dp[j - coin] + 1)
+        for j in range(coin, total + 1):
+            dp[j] = min(dp[j], dp[j - coin] + 1)
 
-  return dp[total] if dp[total] != (total + 1) else -1
-
+    return dp[total] if dp[total] != total + 1 else -1
