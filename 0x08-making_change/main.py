f515d917b4157a7cca04e0c8be84945f6b7f04f8
MoodyEG <moodyeg2011@yahoo.com>
2024-09-20 01:47:52 +0300

    comment

--- 0x08-making_change/main.py
+++ 0x08-making_change/main.py
@@ -9,4 +9,4 @@ print(makeChange([1, 2, 25], 37))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
 
-print(makeChange([3, 1, 4], 10))
+print(makeChange([3, 1, 4], 10))  # with greedy = 4, with dp = 3
