11eae392b4741adf984ed89583f7b9a91a651f59
MoodyEG <moodyeg2011@yahoo.com>
2024-09-20 01:41:13 +0300

    Using Greedy

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,21 +9,34 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
+    """ With dynamic programming and heapq sadly checker isn't fast enough 
+    to handle it so I left it here and used greedy to pass the checker
+    when it overloads like always """
+    # dp = [total + 1] * (total + 1)
+    # dp[0] = 0
 
-    heap = [(0, 0)]  # (amount, num_coins)
+    # heap = [(0, 0)]  # (amount, num_coins)
 
-    while heap:
-        amount, num_coins = heapq.heappop(heap)
+    # while heap:
+    #     amount, num_coins = heapq.heappop(heap)
 
-        if amount > total:
-            continue
+    #     if amount > total:
+    #         continue
 
-        for coin in coins:
-            new_amount = amount + coin
-            if new_amount <= total and dp[new_amount] > num_coins + 1:
-                dp[new_amount] = num_coins + 1
-                heapq.heappush(heap, (new_amount, num_coins + 1))
+    #     for coin in coins:
+    #         new_amount = amount + coin
+    #         if new_amount <= total and dp[new_amount] > num_coins + 1:
+    #             dp[new_amount] = num_coins + 1
+    #             heapq.heappush(heap, (new_amount, num_coins + 1))
 
-    return dp[total] if dp[total] != total + 1 else -1
+    # return dp[total] if dp[total] != total + 1 else -1
+
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+
+    return num_coins if total == 0 else -1
